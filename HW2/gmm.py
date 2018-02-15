5c34a250f5f183f4e33c40f2bb927f6e8739fdb2
Amandeep <ads71993@gmail.com>
2018-02-14 22:24:31 -0500

    gmm extimation updated

--- HW2/gmm.py
+++ HW2/gmm.py
@@ -48,41 +48,41 @@ class GMM(object):
 
         self.mu = self.X[[random.randint(1, self.SIZE) for x in range(self.K)]]
         self.sigma = [np.identity(self.dimension) for i in range(k)]
-        self.p_clus = np.zeros((self.SIZE,self.K))
+        self.p_clus = np.zeros((self.SIZE, self.K))
 
     def file_to_vec(self, path):
         print "Loading data"
         return np.genfromtxt(DATA_DIR + '/' + path, delimiter=' ')
         print "Data loaded"
 
-    def pdf(self,sigma,mu,x):
-        return (1.0/np.sqrt(((2 * np.pi)**self.dimension/2)*np.linalg.det(sigma))) * np.exp(-0.5*np.transpose(x-mu)*np.linalg.inv(sigma)*(x-mu))
+    def pdf(self, sigma, mu, x):
+        return (1.0 / np.sqrt(((2 * np.pi) ** self.dimension / 2) * np.linalg.det(sigma))) * np.exp(
+            -0.5 * (x-mu).dot(np.linalg.inv(sigma)).dot((x-mu).T))
+
 
     def expectation(self):
 
         for i in range(self.SIZE):
-            val = 0.0
             for j in range(self.K):
-                self.p_clus[i][j]= self.weights[j]*self.pdf(sigma=self.sigma[j],mu=self.mu[j],x=self.X[i])
-                self.p_clus[i][j] = self.p_clus[i][j]/np.sum(self.p_clus[i][j])
+                self.p_clus[i][j] = self.weights[j] * self.pdf(sigma=self.sigma[j], mu=self.mu[j], x=self.X[[i]])
+        self.p_clus = self.p_clus / self.p_clus.sum(axis=0,keepdims=True)
 
     def maximisation(self):
         for i in range(self.K):
-            p = self.p_clus[:,i]
-            self.mu[i]=np.sum(p*self.X,axis=0)/np.sum(p)
-            self.weights[i] = np.sum(p)/self.SIZE
-            self.sigma[i]=(1/np.sum(p))*np.sum(p*(self.X-self.mu[i])*np.transpose(self.X-self.mu[i]))
+            p = self.p_clus[:, i]
+            self.mu[i] = np.sum(p * self.X, axis=0) / np.sum(p)
+            self.weights[i] = np.sum(p) / self.SIZE
+            self.sigma[i] = (1 / np.sum(p)) * np.sum(p * (self.X - self.mu[i]) * np.transpose(self.X - self.mu[i]))
 
     def start(self):
         ex = 0
         while ex < self.MAX_ITR:
             self.expectation()
             self.maximisation()
-            ex+=1
+            ex += 1
         print self.mu
 
 
 if __name__ == '__main__':
-    g=GMM()
+    g = GMM()
     g.start()
-
