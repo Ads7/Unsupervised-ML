87b5a343dc4a556b526f2df0ec50034d31d79c80
Amandeep <ads71993@gmail.com>
2018-02-20 02:07:16 -0500

    added templates

--- HW2/dbscan.py
+++ HW2/dbscan.py
@@ -1,4 +1,3 @@
-# class DBSCAN()
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.pairwise import euclidean_distances
 
@@ -13,13 +12,25 @@ NOISE = 99999
 
 
 class DBSCAN(object):
+    attr = ['x', 'y']
 
-    def __init__(self, min_pt= 3, esp=0.3):
+    def __init__(self, min_pt=3, esp=0.3):
+        """
+
+        :param min_pt: int value of min neighbours each point must have to be a core point
+        :param esp: float value of radius of scope of each point
+        """
         self.min_pt = min_pt
         self.esp = esp
 
     def fit(self, vectors):
-        dist_matrix = euclidean_distances(vectors[['x', 'y']].values, vectors[['x', 'y']].values)
+        """
+
+        :type vectors: pandas object having attrs and a column cluster(labels)
+        """
+        # todo can seperate the vectors as numpy array and labels
+        # todo update code as per numpy
+        dist_matrix = euclidean_distances(vectors[self.attr].values, vectors[self.attr].values)
         C = 1
         for i, pt in vectors.iterrows():
             if vectors.at[i, 'cluster'] == NOISE or np.isnan(vectors.at[i, 'cluster']):
@@ -28,7 +39,7 @@ class DBSCAN(object):
                     vectors.at[i, 'cluster'] = NOISE
                 else:
                     vectors.at[i, 'cluster'] = C
-                    q = index  # remove the current point here
+                    q = index
                     while q:
                         j = q.pop()
                         n_pt = vectors.loc[j]
@@ -42,6 +53,12 @@ class DBSCAN(object):
         return pd.read_csv(DATA_DIR + path)
 
     def visualise(self, vectors, name='img.png'):
+        """
+
+        :param vectors: pandas object having labels and vector values
+        :param name: name for the image
+        """
+        # todo visualisation to be updated as for numpy
         plt.figure()
         unique_labels = vectors.cluster.unique().tolist()
         colors = [plt.cm.Spectral(each)
@@ -54,13 +71,71 @@ class DBSCAN(object):
                      vectors[vectors.cluster == k].y.values.tolist(),
                      'o', markerfacecolor=tuple(col),
                      markeredgecolor='k', markersize=14)
-        plt.savefig(name)
+        plt.savefig("img/" + name)
+
+    def cal_purity(self, K, prediction, labels):
+        """
+        :return: : returns the purity and the gini coeff
+        :rtype: float,float
+        """
+        # cal_purity(gmm.predict(FMNIST[:, :-1]), 2, FMNIST[:, [-1]], FMNIST[:, [-1]].shape[0], True)
+        purity = 0.0
+        SIZE = labels.shape[0]
+        for i in range(K):
+            indexes = np.argwhere(prediction == i)
+            counts = np.bincount(labels[indexes.transpose()[0]])
+            purity += counts.max()
+        purity = purity / SIZE
+        return purity
 
 
 class TOYDBSCAN(DBSCAN):
 
     def read(self, path):
         data = pd.read_csv(DATA_DIR + path)
+        data['cluster'] = np.nan
+        return data
+
+
+# todo complete working of household, fashion and NG20
+class HOUSEDATA(DBSCAN):
+    # Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;
+    # Sub_metering_3
+    attr = ['x', 'Global_reactive_power', 'Voltage',
+            'Global_intensity']
+
+    def read(self, path):
+        # .(global_active_power * 1000 / 60 - sub_metering_1 - sub_metering_2 - sub_metering_3)
+        data = pd.read_csv(DATA_DIR + path, sep=';')
+        data['x'] = data['Global_active_power'] * 1000 / 60 - (
+                data['Sub_metering_1'] + data['Sub_metering_2'] + data['Sub_metering_3'])
+        data['cluster'] = np.nan
+        return data
+
+
+class NG20(DBSCAN):
+    # Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;
+    # Sub_metering_3
+    attr = ['x', 'Global_reactive_power', 'Voltage',
+            'Global_intensity']
+
+    def read(self, path):
+        data = pd.read_csv(DATA_DIR + path)
+        data['cluster'] = np.nan
+        return data
+
+
+class FASHIOND(DBSCAN):
+    # Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;
+    # Sub_metering_3
+    attr = ['x', 'Global_reactive_power', 'Voltage',
+            'Global_intensity']
+
+    def read(self, path):
+        # .(global_active_power * 1000 / 60 - sub_metering_1 - sub_metering_2 - sub_metering_3)
+        data = pd.read_csv(DATA_DIR + path, sep=';')
+        data['x'] = data['Global_active_power'] * 1000 / 60 - (
+                data['Sub_metering_1'] + data['Sub_metering_2'] + data['Sub_metering_3'])
         data['cluster'] = np.nan
         return data
 
@@ -84,9 +159,9 @@ if __name__ == '__main__':
     print("DBSCAN moon data")
     dbs = TOYDBSCAN(min_pt=2)
     vectors = dbs.fit(dbs.read('/moons.csv'))
-    dbs.visualise(vectors,'moon.png')
+    dbs.visualise(vectors, 'moon.png')
     print("DBSCAN circle data")
-    dbs = TOYDBSCAN(min_pt=2,esp=0.18)
+    dbs = TOYDBSCAN(min_pt=2, esp=0.18)
     vectors = dbs.fit(dbs.read('/circle.csv'))
     dbs.visualise(vectors, 'circle.png')
     print("DBSCAN blob data")
